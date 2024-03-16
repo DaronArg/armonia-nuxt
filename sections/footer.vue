720c63c618c912dffe1cd308827c6319e2b615c8
devprogq <devprog001@gmail.com>
2024-03-15 21:30:48 -0300

    terminado footer y cursos section

---
+++ sections/footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p>Este sitio es creado con mucho por 2024. </p>
+
+        <!-- instagram icon -->
+        <!-- yt icon -->
+        <!-- fb icon -->
+        <!-- pint icon -->
+    </div>
+
+    <div>
+        <ul class="flex justify-center items-center">
+            <li>Media Kit</li>
+            <li>Mis causas</li>
+            <li>Términos de uso</li>
+            <li>Privacidad</li>
+            <li>Contacto</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+</script>
---
+++ sections/grid.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <!-- 6 planillas de cursos grid de 3, 3fr 1fr -->
+        <img src="" alt="" class="">
+        <button>Membresía Círculo Mágico</button>
+
+        <img src="" alt="" class="">
+        <button>Cursos y cerfiticaciones</button>
+
+        <img src="" alt="" class="">
+        <button>Canalización personal</button>
+
+        <img src="" alt="" class="">
+        <button>Coaching personalizado</button>
+
+        <img src="" alt="" class="">
+        <button>butt extra</button>
+
+        <img src="" alt="" class="">
+        <button>Vlogs, Blogs, Podcast</button>
+
+    </div>
+</template>
+
+<script>
+</script>
+
+<style lang="">    
+</style>
